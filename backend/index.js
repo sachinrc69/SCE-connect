5917a3be3021daa6fbe1ca885a2e0734537fab72
sachinrc69 <sachinrc04@gmail.com>
2024-08-29 18:59:38 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const cors = require("cors");
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: "https://sce-connect-3bdd5.web.app/",
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "https://sce-connect-3bdd5.web.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 require("dotenv").config();
 app.use(express.json());
 app.use(cors());
