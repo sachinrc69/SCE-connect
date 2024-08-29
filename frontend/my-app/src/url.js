06d1235f9ba672840a7ae251ddcdf7576606ef02
sachinrc69 <sachinrc04@gmail.com>
2024-08-29 19:15:11 +0530

    backend url

--- frontend/my-app/src/url.js
+++ frontend/my-app/src/url.js
@@ -1,4 +1,3 @@
 // export const url = "http://localhost:8080/";
 // export const url = "https://connect-api-qz7c.onrender.com/";
-export const url =
-  "https://sce-connect-d772p3iji-sachin-ratnu-chavans-projects.vercel.app/";
+export const url = "https://sce-connect.vercel.app/";
